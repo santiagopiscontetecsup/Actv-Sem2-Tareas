04a0c1aa9cda15a8aca79e09ad03e13c091ea46b
santiagopiscontetecsup <santiago.pisconte@tecsup.edu.pe>
2025-03-26 09:19:32 -0500

    Proyecto Creado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Actv_Sem2_TareaGestion.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
